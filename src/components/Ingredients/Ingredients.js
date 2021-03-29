e4da6b87e76f8f88b15362492c90f8d88d259903
arthurs14 <arthurcs426@gmail.com>
2021-03-29 15:15:57 -0700

    added useEffect to our ingredients component, however a loop occurs when setting ingredients

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -6,6 +6,22 @@ import Search from './Search';
 
 const Ingredients = () => {
   const [ingredients, setIngredients] = useState([]);
+
+  useEffect(() => {
+    fetch('https://ingredient-list-34639-default-rtdb.firebaseio.com/ingredients.json'
+    ).then(response => response.json()
+    ).then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount,
+        });
+      }
+      //setIngredients(loadedIngredients);
+    });
+  });
 
   const addIngredientHandler = ingredient => {
     fetch('https://ingredient-list-34639-default-rtdb.firebaseio.com/ingredients.json', {
