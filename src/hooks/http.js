0d850a6828b9afdcfcb72e2b7f7f117104532e24
arthurs14 <arthurcs426@gmail.com>
2021-04-05 14:20:56 -0700

    created a new file for custom hooks

---
+++ src/hooks/http.js
@@ -0,0 +1,53 @@
+import { useReducer } from 'react';
+
+const httpReducer = (httpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { loading: true, error: null, data: null };
+    case 'RESPONSE':
+      return { ...httpState, loading: false, data: action.responseData };
+    case 'ERROR':
+      return { loading: false, error: action.error };
+    case 'CLEAR':
+      return { ...httpState, error: null };
+    default:
+      throw new Error('Should not be reached!');
+  }
+};
+
+const useHttp = () => {
+  const [httpState, dispatchHttp] = useReducer(
+    httpReducer, 
+    { loading: false, error: null, data: null },
+  );
+
+  const sendRequest = (url, method, body) => {
+    dispatchHttp({ type: 'SEND' });
+
+    fetch (
+      url,
+      {
+        method: method,
+        body: body,
+        header: {
+          'Content-Type': 'application/json'
+        }
+      }).then(response => {
+        return response.json;
+      }).then(responseData => {
+        dispatchHttp({ type: 'RESPONSE', responseData: responseData });
+      }).catch(error => {
+        // setError(error.message);
+        dispatchHttp({ type: 'ERROR', error: error.message  });
+      });
+  };
+
+  return {
+    isLoading: httpState.loading,
+    data: httpState.data,
+    error: httpState.error,
+  };
+
+};
+
+export default useHttp;
